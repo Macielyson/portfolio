5d7d935c5034341390e17e010afc8957ae6575f9
Macielyson Nascimento <macielyson.s@gmail.com>
2023-03-02 17:35:20 -0300

    section portfolio

---
+++ assets/js/script.js
@@ -0,0 +1,8 @@
+function initParallax() {
+    $('#intro').parallax("100%", 0.3);
+    $('#servico').parallax("100%", 0.3);
+    $('#aboutimg').parallax("100%", 0.3);
+    $('#testimonials').parallax("100%", 0.1);
+
+}
+initParallax();
