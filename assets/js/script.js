0f535af2be10d561b82463823fef1126a03cb001
Macielyson Nascimento <macielyson.s@gmail.com>
2023-03-13 20:31:36 -0300

    ajustes em header

--- assets/js/script.js
+++
@@ -1,8 +0,0 @@
-function initParallax() {
-    $('#intro').parallax("100%", 0.3);
-    $('#servico').parallax("100%", 0.3);
-    $('#aboutimg').parallax("100%", 0.3);
-    $('#testimonials').parallax("100%", 0.1);
-
-}
-initParallax();
